d5b7866a54534dccd2a6569803174d4622bee799
Hasnain Baxamoosa <hbaxamoosa@gmail.com>
2020-01-19 10:37:33 -0600

    Updated SettingsActivity to setSummary on selection change.
    Updated GameFragment to take user to InfoActivity when game difficulty of medium.

--- app/src/main/java/com/boxofm/crazytiles/game/GameFragment.kt
+++ app/src/main/java/com/boxofm/crazytiles/game/GameFragment.kt
@@ -1,5 +1,6 @@
 package com.boxofm.crazytiles.game
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,8 +10,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.preference.PreferenceManager
 import com.boxofm.crazytiles.R
 import com.boxofm.crazytiles.databinding.FragmentGame2x2Binding
+import timber.log.Timber
 
 class GameFragment : Fragment() {
 
@@ -18,6 +21,19 @@ class GameFragment : Fragment() {
     private lateinit var binding: FragmentGame2x2Binding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val sharedPref: SharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(activity)
+
+        when (sharedPref.getString("list_preference", "unknown")) {
+            ("Easy") -> Timber.v("%s %s", "game level is ", "Easy")
+            ("Medium") -> {
+                Timber.v("%s %s", "game level is ", "Medium")
+                findNavController().navigate(R.id.infoActivity)
+            }
+            ("Hard") -> Timber.v("%s %s", "game level is ", "Hard")
+            else -> Timber.v("%s %s", "game level is ", "unknown")
+        }
 
         val binding = DataBindingUtil.inflate<FragmentGame2x2Binding>(
                 inflater, R.layout.fragment_game_2x2, container, false
--- app/src/main/java/com/boxofm/crazytiles/ui/SettingsActivity.kt
+++ app/src/main/java/com/boxofm/crazytiles/ui/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.boxofm.crazytiles.ui
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.boxofm.crazytiles.R
 
@@ -17,9 +19,29 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.preferences, rootKey)
+        }
+
+        override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+            val stringValue: String = key.toString()
+            val selection: String = sharedPreferences?.getString(key, "unknown")!!
+
+            if (stringValue == "list_preference") {
+                val listPreference = findPreference<ListPreference>(key.toString())!!
+                listPreference.summary = selection
+            }
+        }
+
+        override fun onResume() {
+            super.onResume()
+            preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+        }
+
+        override fun onPause() {
+            super.onPause()
+            preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
         }
     }
 }
